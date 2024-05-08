51f4153ec5bc1c9dddf7944ea708ab94dc6dcf84
danny207024 <113818186+danny207024@users.noreply.github.com>
2024-05-09 00:23:57 +0800

    Add files via upload

--- public/scripts/score.js
+++ public/scripts/score.js
@@ -7,7 +7,8 @@ const gameScore = function(playerno) {
     // The element holding the score
 
     // The update display function
-    const updateDisplay = function(increase) {
+    const updateDisplay = function(Score) {
+		/*
         if (increase){
             // turn a heart to red
             // Change the fill attribute of the first path element
@@ -27,13 +28,48 @@ const gameScore = function(playerno) {
             $("#heart"+String(playerno)+String(score+1)+" path").eq(2).attr("fill","#FFFFFF");
         }
     };
+		*/
+		
+		for (let i = 1 ;i <=5;i++){
+			
+			  // Change the fill attribute of the first path element
+            $("#heart"+String(playerno)+String(i)+" path").eq(0).attr("fill","#CCCCCC");
+            // Change the fill attribute of the second path element
+            $("#heart"+String(playerno)+String(i)+" path").eq(1).attr("fill","#808080");
+            // Change the fill attribute of the third path element
+            $("#heart"+String(playerno)+String(i)+" path").eq(2).attr("fill","#FFFFFF");
+			
 
+		}
+		
+		for (let i =1 ;i <=Score;i++){
+			
+			  // Change the fill attribute of the first path element
+			$("#heart"+String(playerno)+String(i)+" path").eq(0).attr("fill","#F44336");
+            // Change the fill attribute of the second path element
+            $("#heart"+String(playerno)+String(i)+" path").eq(1).attr("fill","#C62828");
+            // Change the fill attribute of the third path element
+            $("#heart"+String(playerno)+String(i)+" path").eq(2).attr("fill","#FF847A");
+			
+
+		}
+		
+	};
+		
+		
+		
+		
+		
+		
+		
     // The increase score function
     const update = function(effect) {
         score = score + effect;
         if (score < 0){
             score = 0;
         }
+		updateDisplay(score);
+		/*
         else{
             if (effect > 0){
                 updateDisplay(true);
@@ -42,6 +78,10 @@ const gameScore = function(playerno) {
                 updateDisplay(false);
             }
         }
+		*/
+		
+		
+		
     };
 	
 	const getScore = function(){
